6737cdc28b5a0d57c5529cf4961bc67e41113135
nehayel <walidhossain179@gmail.com>
2025-05-24 19:56:10 +0600

    full css project

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+  $(".slick-slider").slick({
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    arrows: false,
+  });
+
+  $(".slider-courses").slick({
+    dots: false,
+    prevArrow:
+      '<button type="button" class="slick-prev custom-prev"><i class="fa-solid fa-chevron-right"></i></button>',
+    nextArrow:
+      '<button type="button" class="slick-next custom-next"><i class="fa-solid fa-chevron-right"></i></i></button>',
+  });
+});

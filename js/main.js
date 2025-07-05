9eac20510bcae289474546e9a9f883cbf8a9c451
nehayel <walidhossain179@gmail.com>
2025-07-05 22:14:38 +0600

    responsive

--- js/main.js
+++ js/main.js
@@ -4,13 +4,42 @@ $(document).ready(function () {
     autoplay: true,
     autoplaySpeed: 1500,
     arrows: false,
+    slidesToShow: 1,
   });
 
   $(".slider-courses").slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
     dots: false,
     prevArrow:
       '<button type="button" class="slick-prev custom-prev"><i class="fa-solid fa-chevron-left"></i></button>',
     nextArrow:
-      '<button type="button" class="slick-next custom-next"><i class="fa-solid fa-chevron-right"></i></i></button>',
+      '<button type="button" class="slick-next custom-next"><i class="fa-solid fa-chevron-right"></i></button>',
+    responsive: [
+      {
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+      {
+        breakpoint: 479,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
+
+  // mean menu
+  jQuery("#mobile-menu").meanmenu({
+    meanScreenWidth: "767",
+    meanMenuContainer: ".mobile-menu",
   });
 });
